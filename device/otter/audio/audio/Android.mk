946fa81ce0819bce5c0780315ff219a48b09c9ba
Jonathan Clavin <jonclavin@gmail.com>
2012-01-21 18:52:48 -0500

    fix audio breakage

--- device/otter/audio/audio/Android.mk
+++ device/otter/audio/audio/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.otter
+LOCAL_MODULE := audio.primary.omap4430
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
